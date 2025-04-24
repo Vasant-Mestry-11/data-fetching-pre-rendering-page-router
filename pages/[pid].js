8e16d0299f4aa727e451f7901e7f4c76c7763b9b
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-24 11:41:35 +0530

    :zap: :art: getStaticPaths

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,27 +1,53 @@
+import path from "path";
+import fs from "fs/promises";
+
 export default function ProductDetailPage({ loadedProduct }) {
   const { title, description } = loadedProduct;
 
   return (
     <>
-      <p>{title}</p>
+      <h1>{title}</h1>
       <p>{description}</p>
     </>
   );
 }
 
 export async function getStaticProps(context) {
-  console.log(context);
+  // console.log(context);
   const { params } = context;
   const productId = params.pid;
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  const product = data.product.find((p) => p.id === productId);
+  const product = data.products.find((p) => p.id === productId);
 
   return {
     props: {
       loadedProduct: product,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          pid: "p1",
+        },
+      },
+      {
+        params: {
+          pid: "p2",
+        },
+      },
+      {
+        params: {
+          pid: "p3",
+        },
+      },
+    ],
+    fallback: false
   };
 }
