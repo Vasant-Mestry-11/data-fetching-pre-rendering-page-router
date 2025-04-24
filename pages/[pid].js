a8acf81673f08b6c500a07932cbcf2ab76b9a657
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-24 15:15:21 +0530

    :zap: :wrench: loading data dynamically from the file

--- pages/[pid].js
+++ pages/[pid].js
@@ -17,13 +17,20 @@ export default function ProductDetailPage({ loadedProduct }) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data
+}
+
 export async function getStaticProps(context) {
   // console.log(context);
   const { params } = context;
   const productId = params.pid;
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+
+  const data = await getData()
 
   const product = data.products.find((p) => p.id === productId);
 
@@ -35,25 +42,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map(product => product.id)
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      {
-        params: {
-          pid: "p1",
-        },
-      },
-      // {
-      //   params: {
-      //     pid: "p2",
-      //   },
-      // },
-      // {
-      //   params: {
-      //     pid: "p3",
-      //   },
-      // },
-    ],
+    paths: pathWithParams,
     // fallback: true
-    fallback: 'blocking'
+    // fallback: 'blocking'
+    fallback: false,
   };
 }
