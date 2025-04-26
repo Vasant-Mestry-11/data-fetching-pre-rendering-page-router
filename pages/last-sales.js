5383cce6e30834f043efa74e398e76ce2c9f5db3
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-26 15:20:43 +0530

    :zap: :art: useSWR hook implementation

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,34 +1,58 @@
 import { useEffect, useState } from "react"
+import useSWR from "swr";
 
 export default function LastSalesPage() {
 
 
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  
-  
+  const { data, error, isLoading } = useSWR('https://nextjs-demo-c6e7e-default-rtdb.asia-southeast1.firebasedatabase.app/Sales.json');
+
   useEffect(() => {
-    setIsLoading(true)
-    fetch('https://nextjs-demo-c6e7e-default-rtdb.asia-southeast1.firebasedatabase.app/Sales.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedData = [];
-        for (let key in data) {
-          transformedData.push({ id: key, username: data[key].username, volume: data[key].volume })
-        }
-        setSales(transformedData)
-        setIsLoading(false)
-      })
-  }, [])
 
-  if (isLoading) {
+    if (data) {
+      console.log(data)
+      const transformedData = [];
+      for (let key in data) {
+        transformedData.push({ id: key, username: data[key].username, volume: data[key].volume })
+      }
+      setSales(transformedData)
+    }
+  }, [data])
+
+  console.log(data, isLoading)
+
+  // useEffect(() => {
+  //   // setIsLoading(true)
+  //   fetch('https://nextjs-demo-c6e7e-default-rtdb.asia-southeast1.firebasedatabase.app/Sales.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log('data', data)
+  //       const transformedData = [];
+  //       for (let key in data) {
+  //         transformedData.push({ id: key, username: data[key].username, volume: data[key].volume })
+  //       }
+  //       setSales(transformedData)
+  //       // setIsLoading(false)
+  //     })
+  // }, [])
+
+  if (error) {
+    return <p>Failed to fetch sales</p>
+  }
+
+  if (isLoading || !sales) {
     return <p>Loading....</p>
   }
 
-  if (!sales) {
-    return <p>No sales yet</p>
-  }
+  // if (isLoading) {
+  //   return <p>Loading....</p>
+  // }
+
+  // if (!sales) {
+  //   return <p>No sales yet</p>
+  // }
 
   return (<ul>
     {sales.map((sale) => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
